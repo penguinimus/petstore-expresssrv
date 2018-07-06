8e5f20c32c9a2f9344fe3ece717cc8462b6bf489
Shawn <pengudeus@gmail.com>
2018-07-06 04:05:48 +0000

    API added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('./queries.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/api/all', db.getAllPuppies);
 
 module.exports = router;
---
+++ routes/queries.js
@@ -0,0 +1,35 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://ubuntu:testpass@localhost:5432/testdb';
+var db = pgp(connectionString);
+
+// add query functions
+function getAllPuppies(req, res, next) {
+  db.any('select * from company')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL things'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+
+module.exports = {
+   getAllPuppies: getAllPuppies,
+//   getSinglePuppy: getSinglePuppy,
+//   createPuppy: createPuppy,
+//   updatePuppy: updatePuppy,
+//   removePuppy: removePuppy
+};
