f84d1e9362b18005910d49e897cf762e07ae556b
Shawn <pengudeus@gmail.com>
2018-07-10 04:13:50 +0000

    added queries.js

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/all', db.getAllPuppies);
+router.get('/api/people', db.getAllPuppies);
+// router.get('/api/people/:id', db.getSinglePuppy);
+// router.post('/api/people', db.createPuppy);
+// router.put('/api/people/:id', db.updatePuppy);
+// router.delete('/api/people/:id', db.removePuppy);
 
 module.exports = router;
--- routes/queries.js
+++ routes/queries.js
@@ -6,13 +6,12 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-// var connectionString = 'postgres://ubuntu:testpass@localhost:5432/testdb';
 var connectionString = process.env.DATABASE_URL;
 var db = pgp(connectionString);
 
 // add query functions
-function getAllPuppies(req, res, next) {
-  db.any('select * from company')
+function getAllPeople(req, res, next) {
+  db.any('select $1:name from $2:name',['*', 'op_users'])
     .then(function (data) {
       res.status(200)
         .json({
@@ -26,9 +25,57 @@ function getAllPuppies(req, res, next) {
     });
 }
 
+// function getSinglePerson(req, res, next) {
+//   var pupID = parseInt(req.params.id);
+//   db.one('select * from company where id = $1', pupID)
+//     .then(function (data) {
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           data: data,
+//           message: 'Retrieved ONE puppy'
+//         });
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
+
+// function createPuppy(req, res, next) {
+//   req.body.age = parseInt(req.body.age);
+//   db.none('insert into pups(name, breed, age, sex)' +
+//       'values(${name}, ${breed}, ${age}, ${sex})',
+//     req.body)
+//     .then(function () {
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           message: 'Inserted one puppy'
+//         });
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
+
+// function updatePuppy(req, res, next) {
+//   db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
+//     [req.body.name, req.body.breed, parseInt(req.body.age),
+//       req.body.sex, parseInt(req.params.id)])
+//     .then(function () {
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           message: 'Updated puppy'
+//         });
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
 
 module.exports = {
-   getAllPuppies: getAllPuppies,
+   getAllPuppies: getAllPeople,
 //   getSinglePuppy: getSinglePuppy,
 //   createPuppy: createPuppy,
 //   updatePuppy: updatePuppy,
